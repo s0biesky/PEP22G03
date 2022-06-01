763fbd6602c9b80d45f45ce74ab1a25921fdaf7c
s0biesky <raul_topala@yahoo.com>
2022-06-01 23:13:47 +0300

    Part1

---
+++ Modul1/part1.py
@@ -0,0 +1,18 @@
+# print('Hello World!\n',end='#')
+
+# print('\n###')
+# print('# #')
+# print('###')
+
+# print('#'*30)
+# print('#',' '*28,'#',sep='')
+# print('#'*30)
+
+# var1 = "Test"
+# var2 = 5
+# print(var1*var2)
+
+var1 = "#"*30
+var2 = "#"+" "*28+"#"
+print(var1,var2,var1,sep="\n")
+
